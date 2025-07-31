b6c97010b813d33e7c6571a4ccc2f7c971072ca7
jarlsbergCheese <fyoosh254@gmail.com>
2025-07-31 10:07:26 -0400

    clicker gameeee

--- clickgame/game.js
+++ clickgame/game.js
@@ -19,17 +19,20 @@ function getRandomInteger(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-// ITEM HAVERS
+// minnimum wage item tracker
+let gompeiWage = false
 
+// ITEM HAVERS:
+// keeps track of the items the player has purchased, and the items available within the game
 let playerItems = [];
-let itemList = [document.getElementById("lawn-mower"), document.getElementById("gompei-day-care"), document.getElementById("mega-mower"), document.getElementById("adoption-papers"), document.getElementById("brick-mowers")];
+let itemList = [document.getElementById("lawn-mower"), document.getElementById("gompei-day-care"), document.getElementById("real-estate-agent"), document.getElementById("adoption-papers"), document.getElementById("brick-mowers"), document.getElementById("mega-mower"), document.getElementById("minnimum-wage"), document.getElementById("high-school-diplomas")];
 
-
-
+// Initialize the score and Gompei counts
 let score = 5;
 let super_gompei_count = 0;
 let ultra_gompei_count = 0;
 
+// Whenever points are added or removed, this function updates all the necesary fields
 function changeScore(amount) {
     score += amount;
     score_element.innerHTML = "Score: " + score;
@@ -46,6 +49,8 @@ function changeScore(amount) {
 
 }
 
+// This function updates the costs of all the widgets in the game, based on how many of each widget the player has purchased
+
 function updateCosts() {
     let lawn = document.getElementById('lawn');
     lawn.setAttribute("cost", lawn.getAttribute("base-cost") ** (1 + ((lawn_container.children.length) * 0.10)));
@@ -55,9 +60,15 @@ function updateCosts() {
     baby_gompei.setAttribute("cost", baby_gompei.getAttribute("base-cost") ** (1 + ((baby_gompei_container.children.length) * 0.10)));
     document.getElementById("baby-gompei-pricetag").innerHTML = Math.floor(baby_gompei.getAttribute("cost")) + " points"
 
+    // Minnimum Wage Item effects the cost of Gompei
     let gompei = document.getElementById('gompei');
-    gompei.setAttribute("cost", gompei.getAttribute("base-cost") ** (1 + ((gompei_container.children.length) * 0.10)));
-    document.getElementById("gompei-pricetag").innerHTML = Math.floor(gompei.getAttribute("cost")) + " points"
+    if (!gompeiWage) {
+        gompei.setAttribute("cost", gompei.getAttribute("base-cost") ** (1 + ((gompei_container.children.length) * 0.10)));
+        document.getElementById("gompei-pricetag").innerHTML = Math.floor(gompei.getAttribute("cost")) + " points"
+    } else {
+        gompei.setAttribute("cost", (gompei.getAttribute("base-cost") ** (1 + ((gompei_container.children.length) * 0.10))) /2 );
+        document.getElementById("gompei-pricetag").innerHTML = Math.floor(gompei.getAttribute("cost")) + " points <br> (Minnimum Wage)"
+    }
 
     let brick_road = document.getElementById('brick-road');
     brick_road.setAttribute("cost", brick_road.getAttribute("base-cost") ** (1 + ((brick_container.children.length) * 0.10)));
@@ -72,6 +83,8 @@ function updateCosts() {
     document.getElementById("ultra-gompei-pricetag").innerHTML = Math.floor(ultra_gompei_price.getAttribute("cost")) + " points"
 }
 
+// This function is called in the onclick() function for each of the widgets, allowing them to be purchased from the store
+// This function only works for widgets, not Items
 
 function buy(store) {
 
@@ -82,15 +95,17 @@ function buy(store) {
     }
     changeScore(-cost);
 
-    let super_gompei = document.querySelector("#super-gompei-container #super-gompei")?.parentElement
-    if (store.getAttribute("name") == "Super-Gompei" && super_gompei != null) {
-        let old_yield = parseInt(super_gompei.getAttribute("reap"));
-        super_gompei.setAttribute("reap", old_yield + 100)
-        super_gompei_count += 1
-        document.body.style = "--gompei-count:" + super_gompei_count
-        return;
-    }
+    // // Handles super-gompei stacking
+    // let super_gompei = document.querySelector("#super-gompei-container #super-gompei")?.parentElement
+    // if (store.getAttribute("name") == "Super-Gompei" && super_gompei != null) {
+    //     let old_yield = parseInt(super_gompei.getAttribute("reap"));
+    //     super_gompei.setAttribute("reap", old_yield + 1000)
+    //     super_gompei_count += 1
+    //     document.body.style = "--gompei-count:" + super_gompei_count
+    //     return;
+    // }
 
+    // handles ultra-gompei stacking
     let ultra_gompei = document.querySelector("#ultra-gompei-container #ultra-gompei")?.parentElement
     if (store.getAttribute("name") == "Ultra-Gompei" && ultra_gompei != null) {
         let old_yield = parseInt(ultra_gompei.getAttribute("reap"));
@@ -102,38 +117,57 @@ function buy(store) {
 
     let new_widget = store.firstElementChild.cloneNode(true);
 
-    // gompei daycare
+    // High school diplomas is placed before gompei daycare so that a baby gompei can not be upgraded twice into a super gompei and break the containers
+
+    // high school diploma item
+    if (playerItems[7] == "true" && new_widget.getAttribute("name") == "Gompei" && getRandomInteger(15, 15) == 15) {
+        new_widget = document.getElementById("super-gompei-id").firstElementChild.cloneNode(true);
+    }
+
+    // gompei daycare item
     if (playerItems[1] == "true" && getRandomInteger(1, 10) == 10 && new_widget.getAttribute("name") == "Baby-Gompei") {
         new_widget = document.getElementById("gompei").firstElementChild.cloneNode(true);
     }
+
 
     new_widget.onclick = () => {
         harvest(new_widget);
     }
 
-    // Bricks
-    if (store.getAttribute("name") == "Brick Road") {
+    // Handles super-gompei stacking
+    let super_gompei = document.querySelector("#super-gompei-container #super-gompei")?.parentElement
+    if (store.getAttribute("name") == "Super-Gompei" && super_gompei != null) {
+        let old_yield = parseInt(super_gompei.getAttribute("reap"));
+        super_gompei.setAttribute("reap", old_yield + 1000)
+        super_gompei_count += 1
+        document.body.style = "--gompei-count:" + super_gompei_count
+        return;
+    }
+
+    // The following code handles the placement of the new widget in the correct container based on its type
+    if (store.getAttribute("name") == "Brick-Road") {
         brick_container.appendChild(new_widget)
     }
-    // Lawn
     if (store.getAttribute("name") == "Lawn") {
         lawn_container.appendChild(new_widget)
     }
     if (store.getAttribute("name") == "Baby-Gompei" && new_widget.getAttribute("name") != "Gompei") {
         baby_gompei_container.appendChild(new_widget)
     }
+    // Gomepei Daycare item
     else if (store.getAttribute("name") == "Baby-Gompei" && playerItems[1] == "true" && new_widget.getAttribute("name") == "Gompei") {
         gompei_container.appendChild(new_widget)
     }
-    // Gompei
-    if (store.getAttribute("name") == "Gompei") {
+    if (store.getAttribute("name") == "Gompei" && new_widget.getAttribute("name") != "Super-Gompei") {
         gompei_container.appendChild(new_widget)
     }
-    // Super
+    // high school diploma item
+    else if(store.getAttribute("name") == "Gompei" && playerItems[7] == "true" && new_widget.getAttribute("name") == "Super-Gompei") {
+        super_gompei_container.appendChild(new_widget)
+    }
     if (store.getAttribute("name") == "Super-Gompei") {
         super_gompei_container.appendChild(new_widget)
     }
-    // Ultra
     if (store.getAttribute("name") == "Ultra-Gompei") {
         ultra_gompei_container.appendChild(new_widget)
         // Initialize a click counter for the rainbow effect
@@ -146,8 +180,26 @@ function buy(store) {
         setup_end_harvest(new_widget)
     }
 
-    if (new_widget.getAttribute("name") == "baby_gompei") {
-        gompei.getAttribute("reap") = gompei.getAttribute("base-reap") * Math.floor(1 + (baby_gompei_container.children.length * 0.10));
+    // adoption papers item
+    if (new_widget.getAttribute("name") == "Gompei" && playerItems[3] == "true") {
+        let baby_gompei = document.getElementById("baby-gompei");
+        baby_gompei.children[0].setAttribute("reap", baby_gompei.children[0].getAttribute("base-reap") * Math.floor(1 + (gompei_container.children.length * 0.25)));
+    }
+
+    // real estate agent item
+    if (new_widget.getAttribute("name") == "Lawn" && playerItems[2] == "true") {
+        let lawn = document.getElementById("lawn");
+        lawn.children[0].setAttribute("reap", lawn.children[0].getAttribute("base-reap") * Math.floor(1 + (lawn_container.children.length * 0.10)));
+        console.log("we did ittt")
+    }
+
+
+    // minnimum wage item
+    if (playerItems[6] == "true" && getRandomInteger(1, 8) == 8) {
+        gompeiWage = true;
+    }
+    if (playerItems[6] == "true" && gompeiWage && new_widget.getAttribute("name") == "Gompei") {
+        gompeiWage = false;
     }
 
     updateReap()
@@ -155,6 +207,7 @@ function buy(store) {
 
 }
 
+// Will determine if auto widgets harvest again after completing a harvest
 function setup_end_harvest(widget) {
     setTimeout(() => {
         widget.removeAttribute("harvesting")
@@ -183,27 +236,20 @@ function harvest(widget, event = false) {
         widget.style.setProperty('--rainbow-intensity', intensity);
     }
 
-    //Lawnmower Effect
-    // console.log(widget.getAttribute("name") )
-    // console.log(lawn_container.children[getRandomInteger(0, lawn_container.children.length - 1)])
-
-    console.log(widget.getAttribute("name"))
-    console.log(playerItems[0])
-
-
     // mower effects
-    if (playerItems[0] == "true" && widget.getAttribute("name") == "Lawn" && !event && getRandomInteger(1, 2) == 2 && playerItems[2] != "true") {
-        console.log("Lawnmower effect triggered")
+    if (playerItems[0] == "true" && widget.getAttribute("name") == "Lawn" && !event && getRandomInteger(1, 2) == 2 && playerItems[5] != "true") {
         harvest(lawn_container.children[getRandomInteger(0, lawn_container.children.length - 1)], true)
     }
-    else if (playerItems[2] == "true" && widget.getAttribute("name") == "Lawn" && !event) {
+    // Mega Mower Effects
+    else if (playerItems[5] == "true" && widget.getAttribute("name") == "Lawn" && !event) {
         harvest(lawn_container.children[getRandomInteger(0, lawn_container.children.length - 1)], true)
         if (getRandomInteger(1, 2) == 2) {
             harvest(lawn_container.children[getRandomInteger(0, lawn_container.children.length - 1)], true)
         }
     }
+
     // Brick Mower
-    if (playerItems[4] == "true" && widget.getAttribute("name") == "Brick Road" && !event) {
+    if (playerItems[4] == "true" && widget.getAttribute("name") == "Brick-Road" && !event) {
         harvest(lawn_container.children[getRandomInteger(0, lawn_container.children.length - 1)], false)
     }
 
@@ -216,6 +262,7 @@ function harvest(widget, event = false) {
 
 }
 
+// In charge of the little pop-up points that appear when a widget is harvested
 function givePoints(widget) {
     let points_element = document.createElement("span")
     points_element.className = "point";
@@ -227,9 +274,9 @@ function givePoints(widget) {
 
 }
 
+// Hides the widget holders when pressed
 function hideHolder(id) {
     let held = document.getElementById(id)
-    // console.log(held.style.display)
     if (held.style.display != "none") {
         held.style.display = "none";
     }
@@ -238,10 +285,12 @@ function hideHolder(id) {
     }
 }
 
+//This function is called to update all of the items the player has bought and not bought
 function setItemsUp() {
-    playerItems = [document.getElementById("lawn-mower").getAttribute("bought"), document.getElementById("gompei-day-care").getAttribute("bought"), document.getElementById("mega-mower").getAttribute("bought"), document.getElementById("adoption-papers").getAttribute("bought"), document.getElementById("brick-mowers").getAttribute("bought")]
+    playerItems = [document.getElementById("lawn-mower").getAttribute("bought"), document.getElementById("gompei-day-care").getAttribute("bought"), document.getElementById("real-estate-agent").getAttribute("bought"), document.getElementById("adoption-papers").getAttribute("bought"), document.getElementById("brick-mowers").getAttribute("bought"), document.getElementById("mega-mower").getAttribute("bought"), document.getElementById("minnimum-wage").getAttribute("bought"), document.getElementById("high-school-diplomas").getAttribute("bought")]
 }
 
+// Seperates the items into the store and the player's items
 function organizeItems() {
 
     yourItems.innerHTML = "";
@@ -259,6 +308,7 @@ function organizeItems() {
 
 }
 
+// The buy function but for items instead of widgets
 function buyItem(item) {
 
     let buying = document.getElementById(item);
@@ -274,17 +324,27 @@ function buyItem(item) {
         setItemsUp();
     }
     organizeItems();
+    updateReap();
 }
 
+// Updates the reap values for all widgets in the game
+// It will update all bought widgets for the player with the appropriate reap value
 function updateReap() {
 
     for (let i = 0; i < store_container.children.length; i++) {
-        for(let t=0; t<store_container.children[i].children.length; t++) {
-            if (store_container.children[i].children[t].classList.contains("reapTag")){
-                store_container.children[i].children[t].innerHTML
+        for (let t = 0; t < store_container.children[i].children.length; t++) {
+            if (store_container.children[i].children[t].classList.contains("reapTag")) {
+                store_container.children[i].children[t].innerHTML = "+" + store_container.children[i].children[0].getAttribute("reap") + " sqft";
             }
         }
     }
+    let holders = document.getElementById("widget-holder")
+    for (let i = 0; i < holders.children.length; i++) {
+        for (let t = 0; t < holders.children[i].children.length; t++) {
+            holders.children[i].children[t].setAttribute("reap", store_container.querySelectorAll('[name=' + holders.children[i].children[t].getAttribute("name") + ']')[0].children[0].getAttribute("reap"));
+        }
+    }
+
 }
 
 
